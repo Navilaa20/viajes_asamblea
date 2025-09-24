cbfb9778ad71cde3ecef37062a4adba1cafe38bb
Tu Nombre <142526447+Navilaa20@users.noreply.github.com>
2025-09-24 12:57:38 -0600

    Agregando usuarios al modelo

--- models.py
+++ models.py
@@ -1,22 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class Usuario(db.Model):
-    __tablename__ = "usuarios"
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # admin o reportador
 
 class TucTuc(db.Model):
-    __tablename__ = "tuc_tuc"
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
+    qr_token = db.Column(db.String(200), unique=True, nullable=False)
 
 class Viaje(db.Model):
-    __tablename__ = "viaje"
     id = db.Column(db.Integer, primary_key=True)
+    tuctuc_id = db.Column(db.Integer, db.ForeignKey('tuc_tuc.id'))
     pasajeros = db.Column(db.Integer, nullable=False)
-    observaciones = db.Column(db.String(255))
-    tuctuc_id = db.Column(db.Integer, db.ForeignKey("tuc_tuc.id"))
+    observaciones = db.Column(db.String(200))
+    fecha = db.Column(db.DateTime, server_default=db.func.now())
